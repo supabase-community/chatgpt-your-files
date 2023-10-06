c2d141c43a4cdb578ac725eced79a8a6b1e672b4
Greg Richardson <greg.nmr@gmail.com>
2023-10-06 17:46:39 -0600

    feat: process documents into subsections

---
+++ supabase/functions/process/index.ts
@@ -0,0 +1,104 @@
+import { createClient } from '@supabase/supabase-js';
+import { processMarkdown } from '../_lib/markdown-parser.ts';
+
+const supabaseUrl = Deno.env.get('SUPABASE_URL');
+const supabaseAnonKey = Deno.env.get('SUPABASE_ANON_KEY');
+
+Deno.serve(async (req) => {
+  if (!supabaseUrl || !supabaseAnonKey) {
+    return new Response(
+      JSON.stringify({
+        error: 'Missing environment variables.',
+      }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  const authorization = req.headers.get('Authorization');
+
+  if (!authorization) {
+    return new Response(
+      JSON.stringify({ error: `No authorization header passed` }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+      headers: {
+        authorization,
+      },
+    },
+    auth: {
+      persistSession: false,
+    },
+  });
+
+  const { document_id } = await req.json();
+
+  const { data: document } = await supabase
+    .from('documents_with_storage_path')
+    .select()
+    .eq('id', document_id)
+    .single();
+
+  if (!document?.storage_object_path) {
+    return new Response(
+      JSON.stringify({ error: 'Failed to find uploaded document' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  const { data: file } = await supabase.storage
+    .from('files')
+    .download(document.storage_object_path);
+
+  if (!file) {
+    return new Response(
+      JSON.stringify({ error: 'Failed to download storage object' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  const fileContents = await file.text();
+  const processedMd = processMarkdown(fileContents);
+
+  const { error } = await supabase.from('document_sections').insert(
+    processedMd.sections.map(({ content }) => ({
+      document_id,
+      content,
+    }))
+  );
+
+  if (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: 'Failed to save document sections' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  console.log(
+    `Saved ${processedMd.sections.length} sections for file '${document.name}'`
+  );
+
+  return new Response(null, {
+    status: 204,
+    headers: { 'Content-Type': 'application/json' },
+  });
+});
---
+++ supabase/migrations/20231006212813_documents.sql
@@ -0,0 +1,120 @@
+create extension if not exists pg_net with schema extensions;
+create extension if not exists vector with schema extensions;
+
+create table documents (
+  id bigint primary key generated always as identity,
+  name text not null,
+  storage_object_id uuid not null references storage.objects (id),
+  created_by uuid not null references auth.users (id) default auth.uid(),
+  created_at timestamp with time zone not null default now()
+);
+
+create view documents_with_storage_path
+with (security_invoker=true)
+as
+  select documents.*, storage.objects.name as storage_object_path
+  from documents
+  join storage.objects
+    on storage.objects.id = documents.storage_object_id;
+
+create table document_sections (
+  id bigint primary key generated always as identity,
+  document_id bigint not null references documents (id),
+  content text not null,
+  embedding vector (384)
+);
+
+create index on document_sections using hnsw (embedding vector_ip_ops);
+
+alter table documents enable row level security;
+alter table document_sections enable row level security;
+
+create policy "Users can insert documents"
+on documents for insert to authenticated with check (
+  auth.uid() = created_by
+);
+
+create policy "Users can query their own documents"
+on documents for select to authenticated using (
+  auth.uid() = created_by
+);
+
+create policy "Users can insert document sections"
+on document_sections for insert to authenticated with check (
+  document_id in (
+    select id
+    from documents
+    where created_by = auth.uid()
+  )
+);
+
+create policy "Users can update their own document sections"
+on document_sections for update to authenticated using (
+  document_id in (
+    select id
+    from documents
+    where created_by = auth.uid()
+  )
+) with check (
+  document_id in (
+    select id
+    from documents
+    where created_by = auth.uid()
+  )
+);
+
+create policy "Users can query their own document sections"
+on document_sections for select to authenticated using (
+  document_id in (
+    select id
+    from documents
+    where created_by = auth.uid()
+  )
+);
+
+create function supabase_url()
+returns text
+language plpgsql
+security definer
+as $$
+declare
+  secret_value text;
+begin
+  select decrypted_secret into secret_value from vault.decrypted_secrets where name = 'supabase_url';
+  return secret_value;
+end;
+$$;
+
+create function private.handle_storage_update() 
+returns trigger 
+language plpgsql
+as $$
+declare
+  document_id bigint;
+  result int;
+begin
+  insert into documents (name, storage_object_id, created_by)
+    values (new.path_tokens[2], new.id, new.owner)
+    returning id into document_id;
+
+  select
+    net.http_post(
+      url := supabase_url() || '/functions/v1/process',
+      headers := jsonb_build_object(
+        'Content-Type', 'application/json',
+        'Authorization', current_setting('request.headers')::json->>'authorization'
+      ),
+      body := jsonb_build_object(
+        'document_id', document_id
+      )
+    )
+  into result;
+
+  return null;
+end;
+$$;
+
+create trigger on_file_upload
+  after insert on storage.objects
+  for each row
+  execute procedure private.handle_storage_update();
