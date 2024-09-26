32747809cde29f411b3a27e1023993a4ab39b2fa
Taylor Rogers <taylor.rogers@lightcast.io>
2024-09-26 15:25:41 -0700

    Actually return a response 🤦

--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -1,43 +1,42 @@
-import { createClient } from '@supabase/supabase-js';
-import { Database } from '../_lib/database.ts';
-
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../_lib/database.ts";
 
 // These are automatically injected
-const supabaseUrl = Deno.env.get('SUPABASE_URL');
-const supabaseAnonKey = Deno.env.get('SUPABASE_ANON_KEY');
+const supabaseUrl = Deno.env.get("SUPABASE_URL");
+const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
 
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers':
-    'authorization, x-client-info, apikey, content-type',
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
 };
 
 Deno.serve(async (req) => {
   // Handle CORS
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders });
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
   }
   if (!supabaseUrl || !supabaseAnonKey) {
     return new Response(
       JSON.stringify({
-        error: 'Missing environment variables.',
+        error: "Missing environment variables.",
       }),
       {
         status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      }
+        headers: { "Content-Type": "application/json" },
+      },
     );
   }
 
-  const authorization = req.headers.get('Authorization');
+  const authorization = req.headers.get("Authorization");
 
   if (!authorization) {
     return new Response(
       JSON.stringify({ error: `No authorization header passed` }),
       {
         status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      }
+        headers: { "Content-Type": "application/json" },
+      },
     );
   }
 
@@ -55,11 +54,11 @@ Deno.serve(async (req) => {
   const { messages, embedding } = await req.json();
 
   const { data: documents, error: matchError } = await supabase
-    .rpc('match_document_sections', {
+    .rpc("match_document_sections", {
       embedding,
       match_threshold: 0.8,
     })
-    .select('content')
+    .select("content")
     .limit(5);
 
   if (matchError) {
@@ -67,22 +66,25 @@ Deno.serve(async (req) => {
 
     return new Response(
       JSON.stringify({
-        error: 'There was an error reading your documents, please try again.',
+        error: "There was an error reading your documents, please try again.",
       }),
       {
         status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      }
+        headers: { "Content-Type": "application/json" },
+      },
     );
   }
 
   const injectedDocs =
     documents && documents.length > 0
-      ? documents.map(({ content }) => content).join('\n\n')
-      : 'No documents found';
+      ? documents.map(({ content }) => content).join("\n\n")
+      : "No documents found";
 
   console.log(injectedDocs);
 
-  
-  return injectedDocs;
+  return new Response(JSON.stringify(injectedDocs), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+  injectedDocs;
 });
